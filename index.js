8ee9025671cdee285026e878848b1f64860532d3
HippoInWindow20 <shippo.hsu@gmail.com>
2022-09-28 08:55:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const http = require('http');
 const { parse } = require('querystring');
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        collectRequestData(req, result => {
-            res.end(`Subject: ${result.subject}, Content: ${result.content}`);
+const server = http.createServer((request, response) => {
+    if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function(data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
         });
-    } else {
-        res.end('');
+
+        request.on('end', function() {
+            var post = parse.parse(body);
+            console.log(post.subject)
+        });
     }
 });
 server.listen(3000);
-
-function collectRequestData(request, callback) {
-    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-    if (request.headers['content-type'] === FORM_URLENCODED) {
-        let body = '';
-        request.on('data', chunk => {
-            body += chunk.toString();
-        });
-        request.on('end', () => {
-            callback(parse(body));
-        });
-    } else {
-        callback(null);
-    }
-}
+console.log('Server started at http://localhost:3000');
