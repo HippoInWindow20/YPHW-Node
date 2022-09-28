05fe837ee54390fa34aa72d1b4104b6a3de5968b
HippoInWindow20 <shippo.hsu@gmail.com>
2022-09-28 08:34:39 +0800

    Initial feature commit

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-var http = require('http');
+const http = require('http');
+const { parse } = require('querystring');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            res.end(`Subject: ${result.subject}, Content: ${result.content}`);
+        });
+    } else {
+        res.end('');
+    }
+});
+server.listen(3000);
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if (request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    } else {
+        callback(null);
+    }
+}
